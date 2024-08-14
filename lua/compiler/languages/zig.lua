333566f90d01358e9fe46d39429462c707c59583
Erik Andersson <erik@lindaroger.com>
2024-08-14 12:14:07 +0000

    Added zig testing

--- lua/compiler/languages/zig.lua
+++ lua/compiler/languages/zig.lua
@@ -8,7 +8,8 @@ local M = {}
 --- Frontend - options displayed on telescope
 M.options = {
   { text = "Zig build and run program", value = "option1" },
-  { text = "Zig build program", value = "option2" }
+  { text = "Zig build program", value = "option2" },
+  { text = "Zig test program", value = "option3" },
 }
 
 --- Backend - overseer tasks performed on option selected
@@ -20,22 +21,59 @@ function M.action(selected_option)
   if selected_option == "option1" then
     local task = overseer.new_task({
       name = "- Zig compiler",
-      strategy = { "orchestrator",
-        tasks = {{ name = "- Build & run program → \"./build.zig\"",
-          cmd = "zig build run " .. arguments ..                             -- compile and run
-                " && echo \"" .. final_message .. "\"",
-          components = { "default_extended" }
-        },},},})
+      strategy = {
+        "orchestrator",
+        tasks = {
+          {
+            name = '- Build & run program → "./build.zig"',
+            cmd = "zig build run "
+              .. arguments -- compile and run
+              .. ' && echo "'
+              .. final_message
+              .. '"',
+            components = { "default_extended" },
+          },
+        },
+      },
+    })
     task:start()
   elseif selected_option == "option2" then
     local task = overseer.new_task({
       name = "- Zig compiler",
-      strategy = { "orchestrator",
-        tasks = {{ name = "- Build program → \"./build.zig\"",
-          cmd = "zig build " .. arguments ..                                 -- compile
-                " && echo \"" .. final_message .. "\"",
-          components = { "default_extended" }
-        },},},})
+      strategy = {
+        "orchestrator",
+        tasks = {
+          {
+            name = '- Build program → "./build.zig"',
+            cmd = "zig build "
+              .. arguments -- compile
+              .. ' && echo "'
+              .. final_message
+              .. '"',
+            components = { "default_extended" },
+          },
+        },
+      },
+    })
+    task:start()
+  elseif selected_option == "option3" then
+    local task = overseer.new_task({
+      name = "- Zig compiler",
+      strategy = {
+        "orchestrator",
+        tasks = {
+          {
+            name = '- Test Program → "./src/tests.zig"',
+            cmd = "zig test src/tests.zig"
+              .. arguments -- compile
+              .. ' && echo "'
+              .. final_message
+              .. '"',
+            components = { "default_extended" },
+          },
+        },
+      },
+    })
     task:start()
   end
 end
